9b8885dac2c0dc34a26fbd1f4550bb702830b783
Nishant Vaity <nishant@ubuntu.ubuntu-domain>
2014-03-23 01:28:22 +0530

    index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 /*
 Plugin Name: Buddypress Market Places
-Plugin URI: http://www.gravatar.com/enishant
-Description: Buddypress Market Places with BePro Listings plugin. Shortcode [marketplaces] to display marketplaces on page.
-Tags: buddypress,marketplaces,BePro Listings
-Version: 1.0
+Plugin URI: https://github.com/enishant/marketplaces
+Description: Marketplaces plugin can be used with Buddypress and BePro Listings for displaying BePro Listings categories as marketplaces on page with shortcode.
+Tags: buddypress,bepro,listing,marketplaces,business, business-directory, catalogue, classifieds, directory,members, real estate
+Version: 1.1
 Author: Nishant Vaity
-Author URI: http://www.gravatar.com/enishant
-License: GPL2
+Author URI: https://github.com/enishant
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 function bp_get_categories() { 
 	$args = array(
@@ -97,4 +98,51 @@ function bp_get_categories() {
 	return $marketplaces;
 }
 add_shortcode('marketplaces','bp_get_categories');
+
+function send_email_on_plugin_activate() {
+	$plugin_title = "Marketplaces";
+	$plugin_url = 'https://wordpress.org/plugins/marketplaces/';
+	$plugin_support_url = 'http://wordpress.org/support/plugin/marketplaces';
+	$plugin_author = 'Nishant Vaity';
+	$plugin_author_url = 'https://github.com/enishant';
+	$plugin_author_mail = 'enishant@gmail.com';
+
+	$website_name  = get_option('blogname');
+	$adminemail = get_option('admin_email');
+	$user = get_user_by( 'email', $adminemail );
+
+	$headers = 'From: ' . $website_name . ' <' . $adminemail . '>' . "\r\n";
+	$subject = "Thank you for installing " . $plugin_title . "!\n";
+	if($user->first_name)
+	{
+		$message = "Dear " . $user->first_name . ",\n\n";
+	}
+	else
+	{
+		$message = "Dear Administrator,\n\n";
+	}
+	$message.= "Thank your for installing " . $plugin_title . " plugin.\n";
+	$message.= "Visit this plugin's site at " . $plugin_url . " \n\n";
+	$message.= "Please write your queries and suggestions at developers support \n" . $plugin_support_url ."\n";
+	$message.= "All the best !\n\n";
+	$message.= "Thanks & Regards,\n";
+	$message.= $plugin_author . "\n";
+	$message.= $plugin_author_url ;
+	wp_mail( $adminemail, $subject, $message,$headers);
+	
+	$subject = $plugin_title . " plugin is installed and activated by website " . get_option('home') ."\n";
+	$message = $plugin_title  . " plugin is installed and activated by website " . get_option('home') ."\n\n";
+	$message.= "Website : " . get_option('home') . "\n";
+	$message.="Email : " . $adminemail . "\n";
+	if($user->first_name)
+	{
+		$message.= "First name : " . $user->first_name . " \n";
+	}
+	if($user->last_name)
+	{
+		$message.= "Last name : " . $user->last_name . "\n";	
+	}
+	wp_mail( $plugin_author_mail , $subject, $message,$headers);
+}
+register_activation_hook( __FILE__, 'send_email_on_plugin_activate' );
 ?>
